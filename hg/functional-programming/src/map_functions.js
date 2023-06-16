9d3a3f02b7513f6ca4bf9d99a337a2ca1fff6aa7
hernan-go <gobulinhernan@gmail.com>
2023-06-16 03:12:18 -0300

    Implemented import statement for 'characters' from 'person_array.js

---
+++ hg/functional-programming/src/map_functions.js
@@ -0,0 +1,18 @@
+const { characters } = require("./person_array.js");
+
+// 1 map function - Get an array of all the names
+const names = characters.map(character => character.name);
+console.log(names);
+
+// 2 map function - Get an array of all the heights
+const heights = characters.map(character => character.height);
+console.log(heights);
+
+// 3 map function - Get an array of objects with the properties name and height
+const nameAndHeight = characters.map(character => ({
+    name: character.name,
+    height: character.height,
+}));
+console.log(nameAndHeight);
+
+// 4 the fourth exercise is the same as the first!
