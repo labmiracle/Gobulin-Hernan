32e32a9f1f92926d9fc7de44cd80478e252723b4
hernan-go <gobulinhernan@gmail.com>
2023-06-23 03:14:22 -0300

    Update Square and Board component to TypeScript

--- hg/react/tic_tac_toe/src/App.tsx
+++ hg/react/tic_tac_toe/src/App.tsx
@@ -1,33 +1,72 @@
 import "./App.css";
+import { useState } from "react";
 
-function Square({ value = "string" }) {
-    function handleClick() {
-        console.log("¡haz hecho clic!");
-    }
+type SquareProps = {
+    value: string | null;
+    onSquareClick: () => void;
+};
+function Square({ value, onSquareClick }: SquareProps) {
     return (
-        <button className="square" onClick={handleClick}>
+        <button className="square" onClick={onSquareClick}>
             {value}
         </button>
     );
 }
 
 export default function Board() {
+    const [xIsNext, setXIsNext] = useState<boolean>(true);
+    const [squares, setSquares] = useState<Array<string | null>>(Array(9).fill(null));
+
+    function handleClick(i: number) {
+        if (squares[i] || calculateWinner(squares)) {
+            return;
+        }
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
+    }
+
+    function calculateWinner(squares: Array<string | null>): string | null {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
     return (
         <>
             <div className="board-row">
-                <Square value="1" />
-                <Square value="2" />
-                <Square value="3" />
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
             </div>
             <div className="board-row">
-                <Square value="4" />
-                <Square value="5" />
-                <Square value="6" />
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
             </div>
             <div className="board-row">
-                <Square value="7" />
-                <Square value="8" />
-                <Square value="9" />
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
             </div>
         </>
     );
