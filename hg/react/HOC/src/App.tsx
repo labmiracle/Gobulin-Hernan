10d91c42e321f7d7a9b6f816bea80713fcc1b3e3
hernan-go <gobulinhernan@gmail.com>
2023-06-27 18:46:19 -0300

    Added component in app file

--- hg/react/HOC/src/App.tsx
+++ hg/react/HOC/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import withPostsData from './components/withPostsData';
 import PostList from './components//PostList';
-
 import './App.css';
+
 const App: React.FC = () => {
 	const PostListWithPostsData = withPostsData(PostList);
 
