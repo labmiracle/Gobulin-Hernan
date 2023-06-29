cf7c3b871d23f311f9f2f7a2074d98f86410f44c
hernan-go <gobulinhernan@gmail.com>
2023-06-29 05:24:15 -0300

    Hook exercises 1-2-3 added

--- hg/Hooks/src/App.tsx
+++ hg/Hooks/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import TextField from './components/Hook1';
+import UnmountComponent from './components/Hook2';
+import ComponentUpdate from './components/Hook3';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+	return (
+		<div>
+			<h1>Hook_1 replaces every letter "a" with the letter "b"</h1>
+			<TextField />
+			<h1>Hook_2 component to be disassembled</h1>
+			<UnmountComponent />
+			<div className='color'>
+				<h2>Hook_3, the component is updated in console after 3 seconds</h2>
+				<ComponentUpdate name='John' />
+			</div>
+		</div>
+	);
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
