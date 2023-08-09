24925c0990aef5ef0a4b6b919f4f772acdbd12df
hernan-go <gobulinhernan@gmail.com>
2023-08-09 15:12:08 -0300

    Server is modified to listen on port 5000

--- hg/node-js/http/app.js
+++ hg/node-js/http/app.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
 	res.end("Hello MiracleLab, we'll see you on Friday at the Happy Hour!");
 });
 
-server.listen(3000, () => {
-	console.log('Server is listening on port 3000');
+server.listen(5000, () => {
+	console.log('Server is listening on port 5000');
 });
