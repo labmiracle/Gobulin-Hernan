cfd0a7fae0636028bcc64b202d227f5e02d29b69
hernan-go <gobulinhernan@gmail.com>
2023-08-09 15:25:07 -0300

    Modify console.log() to retrieve only the URL(/favicon.ico)

--- hg/node-js/http/app.js
+++ hg/node-js/http/app.js
@@ -1,9 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+	console.log(req.url);
 	res.end("Hello MiracleLab, we'll see you on Friday at the Happy Hour!");
 });
 
-server.listen(5000, () => {
-	console.log('Server is listening on port 5000');
+server.listen(3000, () => {
+	console.log('Server is listening on port 3000');
 });
