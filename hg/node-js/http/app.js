4eac057829f2dd2aa2b3d5f57bbc9d923f4661e2
hernan-go <gobulinhernan@gmail.com>
2023-08-09 15:03:30 -0300

    Add instruction to visit localhost:3000 for 'Hello, world' message

---
+++ hg/node-js/http/app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+	res.end('Hello, world!');
+});
+
+server.listen(3000, () => {
+	console.log('Server is listening on port 3000');
+});
