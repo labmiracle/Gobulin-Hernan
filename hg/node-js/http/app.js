eb72478bffa18e13a58227af700406723088130c
hernan-go <gobulinhernan@gmail.com>
2023-08-09 15:07:55 -0300

    The server is modified with a new message

--- hg/node-js/http/app.js
+++ hg/node-js/http/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-	res.end('Hello, world!');
+	res.end("Hello MiracleLab, we'll see you on Friday at the Happy Hour!");
 });
 
 server.listen(3000, () => {
