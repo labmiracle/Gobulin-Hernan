c4bf6586d8250b1dade4c19dc82d22254efe02fa
hernan-go <gobulinhernan@gmail.com>
2023-08-15 04:01:55 -0300

    Remove comments

--- hg/node-js/express/exercise_3/index.js
+++ hg/node-js/express/exercise_3/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const itemsRouter = require('./routes/items'); // Import the items router
+const itemsRouter = require('./routes/items');
 
 app.use(express.json());
 
-app.use('/', express.static('public')); // Serve the static HTML file
+app.use('/', express.static('public'));
 
-app.use('/items', itemsRouter); // Use the items router for item-related routes
+app.use('/items', itemsRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
